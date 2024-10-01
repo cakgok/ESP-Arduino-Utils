d4f2634e900156e6644b0054322378028fa16b75
ca <cemakgok@gmail.com>
2024-10-01 18:20:01 +0200

    init

---
+++ src/ESPUtils.h
@@ -0,0 +1,12 @@
+#ifndef ESP_UTILS_H
+#define ESP_UTILS_H
+
+#include "ESPLogger.h"
+#include "ESPOTASetup.h"
+#include "ESPTelemetry.h"
+#include "ESPTimeSetup.h"
+#include "MQTTManager.h"
+#include "ESPWifi.h"
+// Add other utility headers here as needed
+
+#endif // ESP_UTILS_H
